53344dcec612678eae5fa670cf0b349fd54d2950
Jesus Cova <jesuscovam@gmail.com>
2024-08-18 12:54:43 -0600

    Initial Commit

---
+++ CoffeMasters/CoffeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMastersApp.swift
+//  CoffeMasters
+//
+//  Created by Jesus Cova on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
